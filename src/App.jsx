0a1bf6620285becce017b3f69442134c638705b4
giovannimedici <giovannigilsu@gmail.com>
2024-06-12 20:52:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './styles.css'
+import React from 'react'
+import Gallery from './Gallery'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className='title'>Gio's Gallery</h1>
+      </div>
+      <Gallery />
+    </>
+  )
+}
+
+export default App
